943667cbc731a99c51d0c3eb2426aa785424c87b
post <weixiaoyiri@gmail.com>
2016-09-09 21:44:48 +0800

    flask first day

---
+++ src/hello.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import make_response
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/bad')
+def bad():
+    return '<h1>Bad Request</h1>', 400
+
+@app.route('/response')
+def response():
+    response = make_response('<h1>This document carries a cookies!</h1>')
+    response.set_cookie('answer', '42')
+    return response    
+
+@app.route('/baidu')
+def baidu():
+    return redirect('http://www.baidu.com')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
