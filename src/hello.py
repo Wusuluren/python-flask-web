2769347671dcb65addbd46c7d0815113c225c482
post <weixiaoyiri@gmail.com>
2016-09-10 13:59:24 +0800

    使用flask-wtf处理表单

--- src/hello.py
+++ src/hello.py
@@ -6,11 +6,19 @@ from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route('/')
 def index():
@@ -68,6 +76,16 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('bootstrap_500.html'), 500
+
+@app.route('/bootstrap_form', methods=['GET', 'POST'])
+def bootstrap_form():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = '' 
+    return render_template('bootstrap_form.html', 
+        name=name, form=form)
 
 
 '''
